8c292f19e1a0d2cd36512adffa5a6a648dd6638c
m2b-tiagochaves <tiagodevch@gmail.com>
2021-07-02 18:45:06 -0300

    versao2

--- app.py
+++ app.py
@@ -1,7 +1,11 @@
 from flask import Flask
 
-app = flask(__name__)
+app = Flask(__name__)
 
 @app.route('/')
 def index():
 	return '<h1>teste</h1>'
+
+@app.route('/user/<name>')
+def user(name):
+	return '<h1>Olá {}, bem vindo!'.format(name)
