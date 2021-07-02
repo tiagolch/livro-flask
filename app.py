ed0df810bdd6b816478a441f687340e85d226fb4
m2b-tiagochaves <tiagodevch@gmail.com>
2021-07-02 19:37:10 -0300

    parado na pagina 44

--- app.py
+++ app.py
@@ -1,11 +1,25 @@
 from flask import Flask
+from flask import make_response
+
 
 app = Flask(__name__)
 
 @app.route('/')
 def index():
 	return '<h1>teste</h1>'
+	
 
 @app.route('/user/<name>')
 def user(name):
 	return '<h1>Olá {}, bem vindo!'.format(name)
+
+
+@app.route('/teste')
+def teste():
+	response = make_response('<h1>Este doc carrega um cookie </h1>')
+	response.set_cookie('answer', '42')
+	return response
+
+
+
+app.run(debug=True)
