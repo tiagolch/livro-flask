f7db886f317ae2b830da7287c244941c5c07b207
m2b-tiagochaves <tiagodevch@gmail.com>
2021-07-03 14:55:18 -0300

    Modificado comportamento das paginas e acrescentado paginas de erro

--- app.py
+++ app.py
@@ -1,8 +1,10 @@
 from flask import Flask, render_template
 from flask import make_response
+from flask_bootstrap import Bootstrap
 
 
 app = Flask(__name__)
+bootstrap = Bootstrap(app)
 
 @app.route('/')
 def index():
@@ -11,7 +13,7 @@ def index():
 
 @app.route('/user/<name>')
 def user(name):
-	return '<h1>Olá {}, bem vindo!'.format(name)
+	return render_template('user.html', name=name)
 
 
 @app.route('/teste')
@@ -19,6 +21,16 @@ def teste():
 	response = make_response('<h1>Este doc carrega um cookie </h1>')
 	response.set_cookie('answer', '42')
 	return response
+
+
+@app.errorhandler(404)
+def page_not_found(e):
+    	return render_template('404.html'), 404
+
+
+@app.errorhandler(500)
+def internal_server_error(e):
+    	return render_template('500.html'), 500
 
 
 
